1ca3c40884746456133842279b5818520073aec2
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-11-05 18:53:18 -0400

    finally completed lab

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,92 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private  Character[][] matrix;
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+//        Character[][] board = this.matrix;
+        int xVertCounter = 0;
+        int xHoriCounter = 0;
+        for (int row = 0; row < matrix.length; row++) {
+            xVertCounter = 0;
+            for (int col = 0; col < matrix.length; col++) {
+                if (matrix[col][row] == 'X') { //i want to make the row and col equal to X
+                    xVertCounter++;
+                    if (xVertCounter == 3) {
+                        return true;
+                    }
+                }
+            }
+        }
+        for (int row = 0; row < matrix.length; row++) {
+            xHoriCounter = 0;
+            for (int col = 0; col < matrix.length; col++) {
+                if (matrix[row][col] == 'X') {
+                    xHoriCounter++;
+                    if (xHoriCounter == 3){
+                        return true;
+                    }
+                }
+            }
+        }
+        if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X'
+            || matrix[0][2] == 'X' && matrix[1][1] == 'X' && matrix[2][0] == 'X') {
+            return true;
+        }
+        return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        int oVertiCounter = 0;
+        int oHoriCounter = 0;
+        for (int row = 0; row < matrix.length; row++) {
+            oVertiCounter = 0;
+            for (int col = 0; col < matrix.length; col++) {
+                if (matrix[col][row] == 'O') { //i want to make the row and col equal to X
+                    oVertiCounter++;
+                    if (oVertiCounter == 3) {
+                        return true;
+                    }
+                }
+            }
+        }
+        for (int row = 0; row < matrix.length; row++) {
+            oHoriCounter = 0;
+            for (int col = 0; col < matrix.length; col++) {
+                if (matrix[row][col] == 'O') {
+                    oHoriCounter++;
+                    if (oHoriCounter == 3){
+                        return true;
+                    }
+                }
+            }
+        }
+        if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O'
+                || matrix[0][2] == 'O' && matrix[1][1] == 'O' && matrix[2][0] == 'O') {
+            return true;
+        }
+        return false;
     }
 
     public Boolean isTie() {
-        return null;
+    Boolean result = false;
+    if (isInFavorOfO() == false && isInFavorOfX() == false) {
+        return true;
+    }
+        return result;
+
     }
 
     public String getWinner() {
-        return null;
+        String result = "";
+        if (isInFavorOfX() == true){
+            result = "X";
+        } else if (isInFavorOfO() == true) {
+            result = "O";
+        }
+        return result;
     }
-
 }
